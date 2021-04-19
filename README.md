8c3cda997018a4310667e1c44cf00b191b52a24d
Sejung Kwak <kwak.sejung@gmail.com>
2021-04-19 18:28:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# GitHub Profiles
+
+![screen recording](https://media.giphy.com/media/d1j9jXtviKkexfbGo6/giphy.gif)
+
+#### project notes
+
+1. HTML
+
+- input: text for searchbox "Search a GitHub User"
+- no result: "No profile with this username"
+
+2. CSS
+
+3. JavaScript
+
+- fetch GitHub API, use profile data(picture, name, bio, number of followers, number of following, number of repos, latest 5 repos), display them in a card
+
+- Challenge from Brad Traversy & Florin Pop on Udemy '50 Projects in 50 Days'
+
+#### Takeaways from the instructor
---
+++ script.js
@@ -0,0 +1,74 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const API = 'https://api.github.com/users/';
+const REPO_API = '/repos?sort=updated&direction=desc&per_page=5';
+
+search.focus();
+
+form.addEventListener('submit', (e) => {
+  const searchName = search.value;
+  
+  e.preventDefault();
+
+  if (document.body.lastElementChild.className === 'result') {
+    document.body.lastElementChild.remove();
+  }
+
+  if ( searchName && searchName !== ' ' ) {
+    getUser(API + searchName);
+    search.value = '';
+  } else {
+    window.location.reload();
+  }
+});
+
+function getUser(url) {
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    const div = document.createElement('div');
+    div.classList.add('result');
+    
+    if ( data.message === 'Not Found' ) {
+      const h2 = document.createElement('h2');
+      h2.innerText = 'No profile with this username.'
+      div.appendChild(h2);
+    } else {
+      div.innerHTML = `
+        <img class="avatar" src="${data.avatar_url}" alt="${data.name}" width="100" height="100">
+        <div class="info">
+          <h3 class="name">${data.name}</h3>
+          <p id="bio" class="bio">${data.bio}</p>
+          <div class="figures"></div>
+        </div>
+      `
+    }
+
+    setTimeout(() => {
+      processingNull()
+    }, 100)
+
+    document.body.appendChild(div);
+
+    return fetch(url + REPO_API)
+
+  }).then(res => res.json())
+  .then(results => {
+    const info = document.querySelector('.info')
+    const div = document.createElement('div');
+    div.classList.add('recent-repos');
+
+    results.forEach(result => {
+      div.innerHTML += `<small class="repo">${result.name}</small>`
+    })
+    info.appendChild(div);
+
+  }).catch(err => console.log(err))
+}
+
+function processingNull() {
+  const bio = document.getElementById('bio');
+  if ( bio.textContent === 'null' ) {
+    bio.textContent = 'No bio in this profile.'
+  }
+}
